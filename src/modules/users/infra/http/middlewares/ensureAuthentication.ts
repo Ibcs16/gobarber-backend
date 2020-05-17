130dfb5b2f5d75d76d67fedc449cdd4a2ad41ffd
Ibcs16 <iagobrayhamcunha@hotmail.com>
2020-05-17 16:10:31 -0300

    Entry point and authentication middleware

--- src/modules/users/infra/http/middlewares/ensureAuthentication.ts
+++ src/modules/users/infra/http/middlewares/ensureAuthentication.ts
@@ -1,12 +1,39 @@
 import { Response, Request, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import authConfig from '@config/auth';
+
+interface TokenPayload {
+  iat: string;
+  ext: string;
+  sub: string;
+}
 
 function ensureAuthentication(
-  req: Request,
+  request: Request,
   res: Response,
   next: NextFunction,
 )  {
 
-  next();
+  const authHeader = request.headers.authorization;
+
+  if(!authHeader) {
+    throw Error('JWT token is missing');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+    const { sub } = decoded as TokenPayload;
+    request.user = {
+      id: sub
+    }
+
+    next();
+  } catch {
+    throw new Error('Invalid JWT token');
+  }
+
 }
 
 export default ensureAuthentication;
--- src/shared/server.ts
+++ src/shared/server.ts
@@ -1,9 +1,30 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
 import Routes from './infra/http/routes';
+import upload from '@config/upload';
+import AppError from './errors/AppError';
+import './infra/typeorm/index';
 
 const app = express();
 
+app.use(express.json());
+
+app.use('/files', express.static(upload.directory));
 app.use(Routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if(err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
+})
 
 app.listen( 3333, () => {
   console.log('server on 3333')
