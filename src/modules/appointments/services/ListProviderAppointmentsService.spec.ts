dc6a82a5db1eb5c211c94a8391960bb1183c2d3b
Ibcs16 <iagobrayhamcunha@hotmail.com>
2020-06-01 13:09:47 -0300

    Add list provider appointments in day Add mongodb connection

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,36 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+describe('ListProviderDayAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list providers appointmnts by day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      user_id: 'customer-id',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      user_id: 'customer-id',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider-id',
+      year: 2020,
+      month: 5,
+      day: 20,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
